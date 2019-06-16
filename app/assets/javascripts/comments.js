5c2e75ace9dcb744ca70a4ce5ba6617cc5bf67e6
FukayaHiroki <asitanonegif@gmail.com>
2019-06-16 17:20:10 +0900

    add  last_message

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,16 +1,16 @@
 $(function(){
   function buildMessage(message){
     var image = (message.image)? `<image class="lower-message__image" src="${message.image}">`:"";
-    var html = `<div class="massage" data-id="${message.id}">
-                  <div class="massage__infomation">
-                    <div class="massage__infomation__name">
+    var html = `<div class="message" data-message_id="${message.id}">
+                  <div class="message__infomation">
+                    <div class="message__infomation__name">
                     ${message.user_name}
                     </div>
-                    <div class="massage__infomation__date">
+                    <div class="message__infomation__date">
                     ${message.created_at}
                     </div>
                     </div>
-                  <div class="massage__text">
+                  <div class="message__text">
                     <p class="lower-message__content">
                     ${message.content}
                     </p>
@@ -45,14 +45,13 @@ $(function(){
   })
   var reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    var last_message_id = $('.message:last').data('message-id')
+    var last_message_id = $('.message:last').data('message-id');
     $.ajax({
       //ルーティングで設定した通りのURLを指定
-      url: "/groups/:group_id/api/messages",
+      url: "/groups/9/api/messages",
       //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
--- app/controllers/api/message_controller.rb
+++ app/controllers/api/message_controller.rb
@@ -1,7 +1,8 @@
 class Api::MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
-    # 最後のメッセージ指定は？
-    @new_messages = @messages.where("id > ?", params[:id])
-  end
+    respond_to do |format|
+      format.html
+      format.json{@new_messages = @messages.where('id > ?', params[:id])}
+    end
 end
