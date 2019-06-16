f302d7d5ce4da8cb1bf65eaf3ef2299ca6e6d2ae
FukayaHiroki <asitanonegif@gmail.com>
2019-06-16 15:39:55 +0900

    add erorr

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildMessage(message){
     var image = (message.image)? `<image class="lower-message__image" src="${message.image}">`:"";
-    var html = `<div class="massage">
+    var html = `<div class="massage" data-id="${message.id}">
                   <div class="massage__infomation">
                     <div class="massage__infomation__name">
                     ${message.user_name}
@@ -43,4 +43,32 @@ $(function(){
       alert("エラー");
     })
   })
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message:last').data('message-id')
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "/groups/:group_id/api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      messages.each(function(){
+        inserHTML = buildMessage(message);
+        $('.messages').append(inserHTML);
+      })
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 5000);
 });
+
+  // $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast')
--- app/controllers/api/message_controller.rb
+++ app/controllers/api/message_controller.rb
@@ -1,5 +1,7 @@
 class Api::MessagesController < ApplicationController
   def index
+    @group = Group.find(params[:group_id])
+    # 最後のメッセージ指定は？
     @new_messages = @messages.where("id > ?", params[:id])
   end
 end
