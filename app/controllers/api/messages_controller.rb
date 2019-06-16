958aefb24b947fa5972ea9945be52fc52e0c6672
FukayaHiroki <asitanonegif@gmail.com>
2019-06-17 01:04:05 +0900

    delete commentout

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,9 +1,5 @@
 class Api::MessagesController < ApplicationController
   def index
-    # メッセージに入っているgroup_id == 今表示されている、グループのIDって条件
-    # @group = Group.find(params[:group_id])
-    # @messages = @group.messages.includes(:user).where('id > ?', params[:id])
-    # @new_message = Message.where('id > ?', params[:id] and 'group_id == ?', params[:group_id])
     @new_message = Message.where('id > ?', params[:id]).where(group_id: params[:group_id])
     respond_to do |format|
       format.html
